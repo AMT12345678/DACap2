d29e4a2e86d21146de5099bb7801649f106bf5fa
Iury lewin paiva da silva <yurilewin@gmail.com>
2022-06-01 17:30:01 -0300

    Ap2

---
+++ src/main/java/Bean/JogoBean.java
@@ -0,0 +1,90 @@
+package Bean;
+
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import dao.JogoDao;
+import entidade.Jogo;
+
+@ManagedBean
+public class JogoBean {
+	private Jogo jogo = new Jogo();
+	private List<Jogo> lista; 
+
+	
+	public String salvar() { 
+		try {
+			JogoDao.salvar(jogo);
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso",
+					"O " + jogo.getDescricao() + " foi Salvo com sucesso."));
+			jogo = new Jogo();
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error:", "Erro ao salvar o Jogo"));
+		}
+		return "listagem";
+	}
+
+	public String editar() {
+		try {
+			JogoDao.editar(jogo);
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso", jogo.getDescricao() + " foi editado com sucesso"));
+
+		} catch (Exception e) {
+
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Erro ao editar a Jogo"));
+		}
+		return "listagem";
+	}
+
+	public String excluir() {
+		try {
+			JogoDao.excluir(jogo);
+			lista.remove(jogo);
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso", jogo.getDescricao() + " foi excluido com sucesso"));
+
+		} catch (Exception e) {
+
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Error ao excluir o jogo"));
+		}
+		return null;
+	}
+
+	public void buscarPares() {
+		try {
+			List<Integer> numerosPares = JogoDao.buscarValoresPares(jogo);
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso",
+					"Os números pares do jogo " + jogo.getDescricao() + " são: " + numerosPares));
+
+		} catch (Exception e) {
+
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Erro ao mostrar os n�meros pares"));
+		}
+	}
+
+	public Jogo getJogo() {
+		return jogo;
+	}
+
+	public void setJogo(Jogo jogo) {
+		this.jogo = jogo;
+	}
+
+	public List<Jogo> getLista() {
+		if (lista == null) {
+			lista = JogoDao.listar();
+		}
+		return lista;
+	}
+
+	public void setLista(List<Jogo> lista) {
+		this.lista = lista;
+	}
+
+}
---
+++ src/main/java/dao/JogoDao.java
@@ -0,0 +1,52 @@
+package dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import entidade.Jogo;
+import JPAUtil.JPAUtil;
+
+public class JogoDao {
+
+	public static void salvar(Jogo jogo) {
+		EntityManager em = JPAUtil.criarEntityManager();
+		em.getTransaction().begin();
+		em.persist(jogo);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public static void editar(Jogo jogo) {
+		EntityManager em = JPAUtil.criarEntityManager();
+		em.getTransaction().begin();
+		em.merge(jogo);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public static void excluir(Jogo jogo) {
+		EntityManager em = JPAUtil.criarEntityManager();
+		em.getTransaction().begin();
+		jogo = em.find(Jogo.class, jogo.getId());
+		em.remove(jogo);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public static List<Jogo> listar() {
+		EntityManager em = JPAUtil.criarEntityManager();
+		Query q = em.createQuery("select j from Jogo j");
+		List<Jogo> lista = q.getResultList();
+		em.close();
+		return lista;
+	}
+	
+	public static List<Integer> buscarValoresPares(Jogo j){
+		EntityManager em = JPAUtil.criarEntityManager();
+		TypedQuery<Jogo> q = em.createQuery("from Jogo j where j.id = :id", Jogo.class).setParameter("id", j.getId());
+		List<Integer> listaPares = q.getSingleResult().getNumerosPares();
+		em.close();		
+		return listaPares;
+	}
+}
